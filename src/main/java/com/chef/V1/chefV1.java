e444da93d4db202246fac3a75eb3e2aeabe04562
Nakul Mistry <mistrynakul2001@gmail.com>
2025-04-15 01:05:24 +0530

    gitignore mod

--- src/main/java/com/chef/V1/chefV1.java
+++ src/main/java/com/chef/V1/chefV1.java
@@ -2,13 +2,12 @@ package com.chef.V1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
-public class V1Application {
+public class chefV1 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(V1Application.class, args);
+		SpringApplication.run(chefV1.class, args);
 	}
 
 }
